08d297e0946693a44721407edf9c4aa7f38498ff
Konstantin <kobel.konstantin@web.de>
2024-07-27 18:02:35 +0200

    cleaned up data_loading

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 import solara
-from angiography.meta.utils import load_with_solara
+from angiography.meta.utils import load_data
 from angiography.widgets.main_widget import StartOrSelect
 
 @solara.component
 def Page():
-    module_dict = load_with_solara()
+    module_dict = load_data()
     module_select = solara.reactive("Choose")
     start_module = solara.reactive(None)
     StartOrSelect(module_select, start_module, module_dict)
