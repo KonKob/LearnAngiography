8e759739ff6fb05e5da0beed65b96d740718f841
Konstantin <kobel.konstantin@web.de>
2024-08-03 22:25:54 +0200

    gunicorn integration

--- app.py
+++ app.py
@@ -3,8 +3,11 @@ import solara.server.flask
 from solara_app import Page
 
 app = Flask(__name__)
-app.register_blueprint(solara.server.flask.blueprint, url_prefix="/main/")
+app.register_blueprint(solara.server.flask.blueprint)
 
 @app.route("/")
 def load_page():
-    Page()
+    return Page()
+
+if __name__ == '__main__':  # Script executed directly?
+    app.run()
